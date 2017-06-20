0447e6fa72feb0416aa3b67222ebabcbacb3e041
Sebastian Huber <sebastian.huber@embedded-brains.de>
2017-06-20 09:26:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -x
 
-cp br_uid.mk /opt/rtems-4.12/make/custom
+cd rtems-source-builder/rtems
+../source-builder/sb-set-builder --prefix=/opt/rtems-4.12 4.12/rtems-powerpc
+cd ../..
 RTEMS_MAKEFILE_PATH=/opt/rtems-4.12/powerpc-rtems4.12/br_uid make clean install -f Makefile.rtems -C libyaffs2
+cp br_uid.mk /opt/rtems-4.12/make/custom
 make clean install -C libini
 make clean install -C libdemo
+make clean install -C libbed
 make clean install -C libuid
